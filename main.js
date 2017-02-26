cb5aa3b57be2907dddfbb08e0e26dcf485d9ec77
Ioan CHIRIAC <i.chiriac@yahoo.com>
2017-02-26 10:16:20 +0100

    rewrite generator with new api

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
+/*!
+ * Copyright (C) 2017 Ioan CHIRIAC (BSD3 License)
+ * @authors https://github.com/ichiriac/pdomap/graphs/contributors
+ */
+'use strict';
 
-
-var repository  = require('php-reflection');
+var reflection  = require('php-reflection');
 var model       = require('./src/model');
 var output      = require('./src/output');
 var mysql       = require('mysql');
@@ -11,41 +15,44 @@ var mysql       = require('mysql');
  * @property {repository} workspace
  */
 var generator = function(directory) {
-  var self = this;
-  this.workspace = new repository(directory);
-  // log the parsing progress
-  this.workspace.on('progress', function(counter) {
-    if (counter.total > 0) {
-      var progress = Math.round(counter.loaded / counter.total * 100);
-      if (progress !== self.progress) {
-        self.log('progress', progress);
-        self.progress = progress;
-      }
-    }
-  });
+    var self = this;
+    console.log('>> ' + directory);
+    this.workspace = new reflection.Repository(directory, {
+        forkWorker: false
+    });
+    // log the parsing progress
+    this.workspace.on('progress', function(counter) {
+        if (counter.total > 0) {
+            var progress = Math.round(counter.loaded / counter.total * 100);
+            if (progress !== self.progress) {
+                self.log('progress', progress);
+                self.progress = progress;
+            }
+        }
+    });
 };
 
 /**
  * Connecting to the database
  */
 generator.prototype.connect = function(host, port, user, password, database) {
-  this.log('step', 'Connecting to ' + user + '@' + host);
-  this.db = mysql.createConnection({
-    host     : host,
-    port     : port,
-    user     : user,
-    password : password,
-    database : database
-  });
-  return new Promise(function(done, reject) {
-    this.db.connect(function(err) {
-      if (err) {
-        reject(err);
-        return;
-      }
-      done();
+    this.log('step', 'Connecting to ' + user + '@' + host);
+    this.db = mysql.createConnection({
+        host     : host,
+        port     : port,
+        user     : user,
+        password : password,
+        database : database
     });
-  }.bind(this));
+    return new Promise(function(done, reject) {
+        this.db.connect(function(err) {
+            if (err) {
+                reject(err);
+                return;
+            }
+            done();
+        });
+    }.bind(this));
 };
 
 
@@ -53,70 +60,79 @@ generator.prototype.connect = function(host, port, user, password, database) {
  * Scan files and generates output to the specified path
  */
 generator.prototype.generate = function(destination, namespace, sync) {
-  var self = this;
-  if (!destination) destination = this.workspace.directory;
-  return new Promise(function(done, reject) {
-    self.log('step', 'Scan files');
-    self.workspace.scan().then(function() {
-      self.log('step', 'Reading classes');
-      var classes = [];
-      self.workspace.getByType('class').forEach(function(c) {
-        if (!c.doc) return;
-        var model = c.doc.getAnnotation('model');
-        if (model !== null) {
-          classes.push(c);
-        }
-      });
+    var self = this;
+    if (!destination) destination = this.workspace.directory;
+    return new Promise(function(done, reject) {
 
-      // exports
-      var models = {};
-      self.progress = 0;
-      try {
-        classes.forEach(function(c, index) {
-          var progress = Math.round((index + 1) / classes.length * 100);
-          if (progress !== self.progress) {
-            self.log('progress', progress);
-            self.progress = progress;
-          }
-          // read meta
-          var m = new model(c);
-          models[m.name] = m;
-        });
-        for(var m in models) {
-          models[m].checkExtends(models);
-        }
-      } catch(e) {
-        return reject(e);
-      }
-
-
-      // writing files
-      self.log('step', 'Exporting the model');
-      self.progress = 0;
-      var index = 0;
-      var wait = [];
-      for(var m in models) {
-        var out = new output(models[m]);
-        //console.log(destination + '/' + out.model.name + '.php');
-        wait.push(
-            out.render(destination + '/' + out.model.name + '.php', namespace).then(function() {
-            var progress = Math.round(++index / wait.length * 100);
-            if (progress !== self.progress) {
-              self.log('progress', progress);
-              self.progress = progress;
+        self.log('step', 'Scan files');
+        self.workspace.scan().then(function() {
+            self.log('step', 'Reading classes');
+            var classes = [];
+            self.workspace.getByType('class').forEach(function(c) {
+                if (!c.doc) return;
+                var model = c.doc.getAnnotation('model');
+                console.log(model);
+                if (model !== null) {
+                    classes.push(c);
+                }
+            });
+            if (classes.length === 0) {
+                return reject(
+                    new Error(
+                        'No mapping classes found into "' + self.workspace.directory + '"'
+                    )
+                );
             }
-          }, reject)
-        );
-      }
 
-      Promise.all(wait).then(function() {
-        if (sync) {
-          self.log('step', 'Synchronize the database');
-        }
-        done();
-      })
+            // exports
+            var models = {};
+            self.progress = 0;
+            try {
+                classes.forEach(function(c, index) {
+                    var progress = Math.round((index + 1) / classes.length * 100);
+                    if (progress !== self.progress) {
+                        self.log('progress', progress);
+                        self.progress = progress;
+                    }
+                    // read meta
+                    var m = new model(c);
+                    models[m.name] = m;
+                });
+                for(var m in models) {
+                    models[m].checkExtends(models);
+                }
+            } catch(e) {
+                return reject(e);
+            }
+
+
+            // writing files
+            self.log('step', 'Exporting the model');
+            self.progress = 0;
+            var index = 0;
+            var wait = [];
+            for(var m in models) {
+                var out = new output(models[m]);
+                // console.log(destination + '/' + out.model.name + '.php');
+                wait.push(
+                    out.render(destination + '/' + out.model.name + '.php', namespace).then(function() {
+                        var progress = Math.round(++index / wait.length * 100);
+                        if (progress !== self.progress) {
+                            //self.log('progress', progress);
+                            self.progress = progress;
+                        }
+                    })
+                );
+            }
+
+            Promise.all(wait).then(function() {
+                if (sync) {
+                self.log('step', 'Synchronize the database');
+                }
+                done();
+            }, reject);
+        }, reject);
     });
-  });
 };
 
 /**
